546fbf27a5d57ff9aeeab6e0a3ac88eea9a9138d
TorreJohnson <torre.johnson@gmail.com>
2018-01-30 11:50:12 -0500

    completed Sinatra MVC lab

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+
+    @string = params[:user_phrase]
+    pig_latinizer = PigLatinizer.new
+    @pig_word = pig_latinizer.to_pig_latin(@string)
+    erb :pig_latinized
+  end
+
 end
